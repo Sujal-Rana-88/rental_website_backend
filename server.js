2824f382ccedbeba92ef20cc33119c39de8fa256
Sujal Rana <118067837+Sujal-Rana-88@users.noreply.github.com>
2024-11-02 19:39:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,27 +12,33 @@ const path = require("path");
 
 const app = express();
 
-// app.use(cors());
+// List of allowed origins
+const allowedOrigins = ['https://horizon-games.netlify.app', 'https://horizon-games-v1.netlify.app'];
+
 app.use(cors({
-  // origin: 'https://horizon-games.netlify.app/',
-  origin: 'https://horizon-games-v1.netlify.app',
-  credentials: true 
+  origin: (origin, callback) => {
+    // Check if the origin is in the allowedOrigins list or if it's undefined (for non-browser requests)
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
 }));
+
 app.use(helmet());  
 // app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("dev")); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false })); 
 
-
-
 app.use("/", authRoutes);
 app.use("/games", lendRoutes);  
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-  
+
 connectDB(); 
 
 app.listen(port, () => { 
     console.log(`App is running on ${port}`);
 });
-  
