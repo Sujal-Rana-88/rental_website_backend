3cbccacf073f07578ec384b135341597db48f0b3
Sujal Rana <118067837+Sujal-Rana-88@users.noreply.github.com>
2024-12-06 15:36:42 +0530

    added rate limit and updated backend logic and structure

--- Middlewares/authMiddleware.js
+++ Middlewares/authMiddleware.js
@@ -1,16 +1,13 @@
 const verifyToken = (req, res, next) => {
-    const token = req.headers['authorization']?.split(' ')[1]; 
-
+    const token = req.headers['authorization']?.split(' ')[1];
     if (!token) {
         return res.status(401).json({ error: 'Access token is missing' });
     }
 
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
         if (err) {
- 
             return res.status(401).json({ error: 'Invalid or expired token' });
         }
-
         req.userId = decoded.userId;
         next();
     });
--- controllers/LendingGame/LendGamePost.js
+++ controllers/LendingGame/LendGamePost.js
@@ -1,37 +1,42 @@
 const { v4: uuidv4 } = require("uuid");
+const cloudinary = require("../../config/cloudinaryConfig");
 const lendingSchema = require("../../schemas/lendingSchema");
 const { createTable, insertRecord } = require("../../utils/sqlFunctions");
 
 const lendGame = async (req, res) => {
   const { gameName, price, lendingPeriod } = req.body;
   const userId = req.user.userId;
-  const image = req.file ? req.file.filename : null; // Get the uploaded image path
 
   // Check if all required fields are present
-  if (!gameName || !price || !lendingPeriod || !image) {
+  if (!gameName || !price || !lendingPeriod || !req.file) {
     return res.status(400).json({ error: "All fields are required" });
   }
 
   try {
     await createTable(lendingSchema);
+    
+    // Upload image to Cloudinary
+    const result = await cloudinary.uploader.upload(req.file.path, {
+      folder: "lendings",
+    });
 
     const lending = {
       lendingId: uuidv4(),
       gameName,
       price,
       lendingPeriod,
-      image, // Save the image path to the database
+      image: result.secure_url,
       userId,
     };
 
     await insertRecord("lendings", lending);
 
-    return res.status(201).json({ message: "Game lent successfully", image }); // Return image path if needed
+    return res.status(201).json({ message: "Game lent successfully", image: result.secure_url });
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
 };
 
-module.exports = {  
-  lendGame,
+module.exports = {
+  lendGame
 };
